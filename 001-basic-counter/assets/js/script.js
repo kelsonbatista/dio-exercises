011e0c3f2abb47e7639e766a06e1c20a4072805c
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-12-12 22:58:20 -0300

    Basic counter

---
+++ 001-basic-counter/assets/js/script.js
@@ -0,0 +1,23 @@
+const number = document.querySelector('#number');
+const btnSum = document.querySelector('#sum');
+const btnSub = document.querySelector('#sub');
+// let n = number.innerHTML;
+let n1 = 0;
+
+function sum() {
+  n1 += 1;
+  number.innerHTML = n1;
+  if (n1 > 9) btnSum.disabled = true;
+  btnSub.disabled = false;
+}
+
+btnSum.addEventListener('click', sum);
+
+function sub() {
+  n1 -= 1;
+  number.innerHTML = n1;
+  if (n1 <= 0) btnSub.disabled = true;
+  btnSum.disabled = false;
+}
+
+btnSub.addEventListener('click', sub);
