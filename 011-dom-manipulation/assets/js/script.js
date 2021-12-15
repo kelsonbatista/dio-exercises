efe2bd64de2083a49121dc5bed4c0c4f22669d84
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-12-14 21:43:27 -0300

    011 updated

--- 011-dom-manipulation/assets/js/script.js
+++ 011-dom-manipulation/assets/js/script.js
@@ -1,10 +1,15 @@
-const text = document.querySelector('.titulo');
+const text = document.querySelector('.title');
 const btnMode = document.querySelector('.btnMode');
 
 
 const changeMode = () => {
   document.body.classList.toggle('dark-mode');
   btnMode.classList.toggle('dark-mode');
+  if (btnMode.classList.contains('dark-mode')) {
+    text.innerHTML = 'Dark Mode ON';
+  } else {
+    text.innerHTML = 'Light Mode ON';
+  }
 }
 
 btnMode.addEventListener('click', changeMode);
